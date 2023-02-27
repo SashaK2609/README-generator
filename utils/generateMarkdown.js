91ad5950fdbd3060e2b9a745ab6a572d80c189ee
SashaKorobkina <s.dekaiper@gmail.com>
2023-02-27 12:36:23 +0000

    created main titles and references to README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,34 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+by ${data.name}
+${renderLicenseBadge(data.license)};
+## Table of Content
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contribiting)
+* [Tests](#tests)
+* [Questions](#questions)
+${renderLicenseLink(data.license)};
+## Description
+${data.description}
+## Instalation
+${data.instalation}
+## Usage
+${data.usage}
+## License
+This progect is licensed under the ${renderLicenseSection(data.license)} license
+## Contributing
+Contributors: ${data.contributing}
 
+## Tests
+The following is needed to run the test: ${data.tests}
+## Questions
+If you have any questions about the project contact me by:
+- Email - ${data.email}
+- GitHub - [${data.creator}](https://github.com/${data.creator})
 `;
 }
 
