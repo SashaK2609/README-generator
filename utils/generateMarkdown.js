39846130462fa02b51fc0123e2f9fc2b4ce16bcc
SashaKorobkina <s.dekaiper@gmail.com>
2023-02-28 16:19:37 +0000

    improved general view of the file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,15 @@
 //function that will show the badge of the license based on user's choise
 function renderLicenseBadge(license) {
 	if (license !== "none") {
-		return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+		return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
 	} else
-	return "";
+	return ""
 };
-
-//function to create a license link
-// function renderLicenseLink(license) {
-// 	if (license !== "none") {
-// 		return `\n [License](#license)\n`;
-// 	} else
-// 	return "";
-// };
 
 //function that returns license section of README
 function renderLicenseSection(license) {
 	if (license !== "none") {
-		return `## License
-		This progect is licensed under the ${license} license.`;
+		return `This progect is licensed under the ${license} license.`;
 	} else
 	return "";
 };
@@ -32,24 +23,29 @@ ${renderLicenseBadge(data.license)};
 * [Description](#description)
 * [Installation](#installation)
 * [Usage](#usage)
-* [License](#license)
+* [License](#license)      
 * [Contributing](#contribiting)
 * [Tests](#tests)
 * [Questions](#questions)
-////${renderLicenseLink(data.license)};
+
 ## Description
 ${data.description}
+
 ## Instalation
 ${data.instalation}
+
 ## Usage
 ${data.usage}
+
 ## License
 ${renderLicenseSection(data.license)} 
+
 ## Contributing
 Contributors: ${data.contributing}
 
 ## Tests
 The following is needed to run the test: ${data.tests}
+
 ## Questions
 If you have any questions about the project contact me by:
 - Email - ${data.email}
